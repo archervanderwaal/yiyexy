b909680d524c9eae931ee593dd32e9eb321253f2
StormMa <StormMaybin@gmail.com>
2017-05-07 14:30:05 +0800

    重构拼车整合美食

---
+++ src/main/java/com/yiyexy/YiyexyApplication.java
@@ -0,0 +1,12 @@
+package com.yiyexy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiyexyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YiyexyApplication.class, args);
+	}
+}
